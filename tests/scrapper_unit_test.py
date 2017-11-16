7f4f5b6ca1ca0ab0f2666dd351af3077ba72ce81
pmuriuki98 <pmuriuki98@gmail.com>
2017-11-16 02:16:05 -0500

    testsuite idea

---
+++ tests/scrapper_unit_test.py
@@ -0,0 +1,14 @@
+"""test secondary functions. the native functions  that enable the high level functions work"""
+import unittest
+
+class ScrapTests(unittest.Testcase):
+    """refer to module documentation"""
+  
+    def test_find_all(self):
+        """finds the occurence of a string in disregard of presence of special characters in string"""
+        pass
+    
+    def test_over_under(self):
+        """cover all over/under market quotations variations"""
+        
+    
