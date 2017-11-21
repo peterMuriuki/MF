ef84e493e4f6c219d3129d0068dd10dfcdf34f18
= <pmuriuki98@gmail.com>
2017-11-21 10:30:43 +0300

    refactor request requesting

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -7,9 +7,11 @@ def setup_module(module):
     """sets these all up for the full module execution"""
     # we define the application instance here now
     app = create_app('testing')
+    app.config['SERVER_NAME'] = "127.0.0.1"
     app_context = app.app_context()
     app_context.push()
     db.create_all()
+    global client
     client = app.test_client()
 
 def teardown_module(module):
@@ -18,23 +20,37 @@ def teardown_module(module):
     db.drop_all()
 
 
+def test_return_urls():
+    """Get the home route and see if the contents match """
+    response = client.get(url_for('main.default'))
+    res = json.loads(response.data)
+    assert(type(res) is dict)
+
+
 def test_registration():
     """works properly  against devoid information and responds with the required info
     test ->  post data"""
-    data = {
-        "name": "",
-        "email": "",
-        "user_name": "",
-        "password": ""
-    }
-    response = client.post(url_for('user.register'), data=data) 
+    data = '''{
+        "name": "Peter",
+        "email": "Pmuriuki@gmail.com",
+        "user_name": "peter",
+        "password": "adasdwe"
+    }'''
+    # kwani how is one supposed to package a json payload to a post request -> not as from data
+        # answer the data payload containing the json data should be added as type str
+    headers = {
+        'content-type': "application/json",
+        'cache-control': "no-cache"
+        }
+    response = client.post(url_for('user.register'), data=data, headers=headers)
     assert(response.status_code == 201)
     data = json.loads(response.data)
     assert(data['message'] == "User account created succesfully")
-    assert(data['user'] is dict)
+    print(data['user'])
+    assert(type(data['user']) is dict)
 
 def test_registration_from_db():
     """Test the registration mechanism from the database point of view"""
     users = Users.query.all()
-    assert(len(users))
+    assert(bool(len(users)))
 
