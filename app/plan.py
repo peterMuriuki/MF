fa88b2a04a9802d2b12699bf51bf7308f26f3e94
= <pmuriuki98@gmail.com>
2018-01-12 20:43:51 +0300

    add initial plan class methods

--- app/plan.py
+++ app/plan.py
@@ -1,3 +1,54 @@
+class users:
+    """Proposed extension to the users class so that it can support the plans functionality"""
+    def __init__(self):
+        self._bankroll = 0
+        self._plan = ''
+
+    def get_bankroll(self):
+        """bankroll getter method
+        :param None
+        :returns: the bankroll rounded off to the nearest 2dp """
+        return self._bankroll
+
+    def get_plan(self):
+        """ the Plan getter method
+        :param: None
+        :returns: the plan as a string
+        """
+        return self._plan
+
+    def add_bankroll(self, amount):
+        """bankroll setter method
+        :param: the amount as a positive integer or float
+        :return: the updated amount
+        """
+        if isinstance(amount, int) or isinstance(amount, float) and amount >= 0:
+            self._bankroll += amount
+        else:
+            raise ValueError("Unexpected value for bankroll")
+        return self._bankroll
+
+    def sub_bankroll(self, amount):
+        """Bankroll setter method incase we are reducing the bankroll:
+        :param: amount as a positive integer or float
+        : return the updated bankroll"""
+        if isinstance(amount, int) or isinstance(amount, float) and amount >= 0:
+            self._bankroll -= amount
+        else:
+            raise ValueError("Unexpected value for bankroll")
+        return self._bankroll
+
+    def set_plan(self, plan):
+        """Plan setter method
+        :param: a plan as a string
+        :returns None , raises a ValueError if the Process failed"""
+        plans = ['aon', 'ton', 'don']
+        if isinstance(plan, str) and plan in plans:
+            self._plan = plan
+        else:
+            raise ValueError("Unexpected value for plan")
+
+
 class Plans(object):
     """the base class that models all the other plans"""
 
